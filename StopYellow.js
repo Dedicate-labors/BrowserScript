28e297d8f6bda9966a057adfd188d78e360b80ec
Dedicate_labors <47410989+Dedicate-labors@users.noreply.github.com>
2023-11-04 10:02:20 +0800

    Update StopYellow.js
    加入了文字诗词

--- StopYellow.js
+++ StopYellow.js
@@ -26,6 +26,30 @@
     // 将纯青色页面添加到文档中
     document.body.appendChild(bluePage);
 
+    let title = document.createElement("h1");
+    title.style.textAlign = "center";
+    title.innerHTML = "将进酒";
+    bluePage.appendChild(title);
+
+    let p = document.createElement("p");
+    p.style.textAlign = "center";
+    p.style.marginTop = "5em";
+    p.style.marginBottom = "15em";
+    p.innerHTML = "君不见黄河之水天上来，奔流到海不复回。" +
+        "<br>君不见高堂明镜悲白发，朝如青丝暮成雪。"+
+        "<br>人生得意须尽欢，莫使金樽空对月。"+
+        "<br>天生我材必有用，千金散尽还复来。"+
+        "<br>烹羊宰牛且为乐，会须一饮三百杯。"+
+        "<br>岑夫子，丹丘生，将进酒，杯莫停。"+
+        "<br>与君歌一曲，请君为我倾耳听。"+
+        "<br>钟鼓馔玉不足贵，但愿长醉不愿醒。"+
+        "<br>古来圣贤皆寂寞，惟有饮者留其名。"+
+        "<br>陈王昔时宴平乐，斗酒十千恣欢谑。"+
+        "<br>主人何为言少钱，径须沽取对君酌。"+
+        "<br>五花马、千金裘，呼儿将出换美酒，与尔同销万古愁。"
+    p.style.fontSize = "20px";
+    bluePage.appendChild(p)
+
 // =================== 重定向到其他界面 =====================
     // 想跳转的url
     // var redirectUrl = "https://jandan.net/top";
